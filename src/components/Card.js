9f02239573e1a46cdef412e3e449ca8b480a41d3
unknown <aryan8931080555@gmail.com>
2020-07-01 00:25:40 +0530

    Some fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,23 +9,27 @@ export default class Card extends Component {
     this.state={hover:false}
   }
   h=()=>{
-    if(this.state.hover){
-    this.setState({hover:false})}
-    else{
-      this.setState({hover:true})
-    }
+  
+    this.setState({hover:true})
+   
+  }
+  h_=()=>{
+    this.setState({hover:false})
   }
   render() {
+
     return (
       
-      <div className={styles.cardholder} style={{width:this.props.size}}>
+      <div className={styles.cardholder} style={{width:this.props.size}} onMouseOver={this.h} onMouseLeave={this.h_} >
        
        
-<div className={styles.img}  onMouseOver={this.h} onMouseLeave={this.h}></div>
-<label className={styles.eventp}>20$-40$</label>
-        <label className={styles.eventn}>St pter loremp -</label>
-        <button  className={this.state.hover ? styles.eventll : styles.eventl}>Gyanpur Bhadohi</button>
-        <button   className={this.state.hover ? styles.booknow : styles.booknowl}>Book Now  <ArrowRightOutlined /> </button>
+
+<label className={styles.eventp}  onMouseOver={this.h} onMouseLeave={this.h_}>20$-40$</label>
+        <label className={styles.eventn}  onMouseOver={this.h} onMouseLeave={this.h_}>St pter loremp -</label>
+        <button  onMouseOver={this.h} onMouseLeave={this.h_} className={this.state.hover ? styles.eventll:styles.eventl}  >Gyanpur Bhadohi</button>
+    
+      <Link to="/aboutevents">  <button  onMouseOver={this.h} onMouseLeave={this.h_} className={this.state.hover ? styles.booknow : styles.booknowl}>Book Now  <ArrowRightOutlined /> </button></Link>
+   
       </div>
      
     );

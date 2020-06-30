9fb9baaaf3d72e63ad815b5c40745b8298f1a782
unknown <aryan8931080555@gmail.com>
2020-06-30 17:54:14 +0530

    Linked

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styles from './Card.module.css'
 import back from './b1.jpg';
+import {Link} from 'react-router-dom'
 import { ArrowRightOutlined} from '@ant-design/icons';
 export default class Card extends Component {
   constructor(props){
@@ -16,6 +17,7 @@ export default class Card extends Component {
   }
   render() {
     return (
+      <Link to='/aboutevents'>
       <div className={styles.cardholder} style={{width:this.props.size}}>
        
        
@@ -25,6 +27,7 @@ export default class Card extends Component {
         <button  className={this.state.hover ? styles.eventll : styles.eventl}>Gyanpur Bhadohi</button>
         <button   className={this.state.hover ? styles.booknow : styles.booknowl}>Book Now  <ArrowRightOutlined /> </button>
       </div>
+      </Link>
     );
   }
 }

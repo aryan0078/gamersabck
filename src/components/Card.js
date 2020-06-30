75af2631fd9867020cd8e7d97678107377d93bf0
unknown <aryan8931080555@gmail.com>
2020-06-30 13:05:31 +0530

    Some chnages

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import styles from './Card.module.css'
-import back from './b1.jpg'
+import back from './b1.jpg';
+import { ArrowRightOutlined} from '@ant-design/icons';
 export default class Card extends Component {
+  constructor(props){
+    super(props)
+    this.state={hover:false}
+  }
+  h=()=>{
+    if(this.state.hover){
+    this.setState({hover:false})}
+    else{
+      this.setState({hover:true})
+    }
+  }
   render() {
     return (
       <div className={styles.cardholder} style={{width:this.props.size}}>
        
        
-<div className={styles.img}></div>
+<div className={styles.img}  onMouseOver={this.h} onMouseLeave={this.h}></div>
 <label className={styles.eventp}>20$-40$</label>
         <label className={styles.eventn}>St pter loremp -</label>
-        <label className={styles.eventl}>Gyanpur Bhadohi</label>
-        <label className={styles.booknow}>Book Now </label>
+        <button  className={this.state.hover ? styles.eventll : styles.eventl}>Gyanpur Bhadohi</button>
+        <button   className={this.state.hover ? styles.booknow : styles.booknowl}>Book Now  <ArrowRightOutlined /> </button>
       </div>
     );
   }
